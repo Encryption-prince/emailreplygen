4dce9b8a298c612cab6082dda302bbbf64dd4ac9
Encryption-prince <subhammaity6827@gmail.com>
2025-03-01 17:30:16 +0530

    corrected cross origin

--- src/main/java/com/emailreplygenerator/emailreplygen/Controller/EmailGeneratorController.java
+++ src/main/java/com/emailreplygenerator/emailreplygen/Controller/EmailGeneratorController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(value = "*")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/email")
 public class EmailGeneratorController {
 
